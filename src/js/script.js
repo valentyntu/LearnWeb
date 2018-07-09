9abfcd353748162405ca7cb6648d0826b2cc423d
Valentyn Tulub <valentyntu@softwareplanet.uk.com>
2018-07-09 18:08:22 +0300

    Tried to get assignments of employee

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ let databaseURL = "http://localhost:4000/";
 let entityHeader = document.getElementById("entityHeader");
 
 window.onload = function () {
+    load("employees");
     entityHeader.innerText = "";
 };
 
@@ -16,7 +17,9 @@ function get(entities) {
 function show(entityName, collection) {
 
     entityHeader.innerText = getCapitalized(entityName);
-    entityHeader.removeAttribute("hidden");
+
+    let entityJumbotron = document.getElementById("entityJumbotron");
+    entityJumbotron.removeAttribute("hidden");
 
     let entitiesView = document.getElementById("entities");
     let template = document.getElementById(entityName + "Template").content;
@@ -39,6 +42,14 @@ function getCapitalized(string) {
     return string.substring(0, 1).toUpperCase() + string.substring(1);
 }
 
+function getCurrentAssignments(employee) {
+    return fetch(databaseURL + "assignments/?employee_id=" + employee.id).then(r => r.json());
+}
+
+function getCurrentProjects(employee) {
+    return getCurrentAssignments(employee).then(a => (fetch(databaseURL + "projects/?id=" + a.project_id)).then(r => r.json())).then(r => r.json());
+}
+
 function updateTemplate(template, entity, entityName) {
     let entityId = template.querySelector(".id");
     entityId.innerText = entity.id;
@@ -53,6 +64,15 @@ function updateTemplate(template, entity, entityName) {
             entityName.innerText = entity.name;
             let technologies = template.querySelector(".technologies");
             technologies.innerText = entity.technologies;
+            let currentAssignments = template.querySelector(".currentAssignments");
+            let employeesProjects = [];
+            getCurrentProjects(entity).then(p => currentAssignments.innerText += " " + p.name + ",");
+            if (employeesProjects.length === 0) {
+
+            } else {
+
+            }
+
             break;
         }
         case "projects": {
